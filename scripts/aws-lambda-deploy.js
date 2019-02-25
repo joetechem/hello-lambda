e2d8c01c79f1e8d934c859070b7e25ba31519369
joetechem <joe@techemstudios.com>
2019-02-25 12:13:38 -0500

    lambdaName update

--- scripts/aws-lambda-deploy.js
+++ scripts/aws-lambda-deploy.js
@@ -3,8 +3,8 @@ const read = require('fs').readFileSync;
 const AWS = require('aws-sdk');
 const Promise = require('bluebird');
 
-const lambdaName = process.argv[2];
-if (!lambdaName) {
+const hello-world-lambda = process.argv[2];
+if (!hello-world-lambda) {
   console.error('Error: missing lambda name.');
   process.exit(1);
 }
@@ -18,16 +18,16 @@ const execCommand = Promise.promisify(exec);
 
 const cwd = process.cwd();
 const zipLambdaCommand = `
-  cd ${cwd}/lambda/${lambdaName}/ &&
+  cd ${cwd}/lambda/${hello-world-lambda}/ &&
   npm install --production &&
-  zip -r ${lambdaName}.zip * --quiet`;
+  zip -r ${hello-world-lambda}.zip * --quiet`;
 
 execCommand(zipLambdaCommand)
 .then(() => {
   const lambdaUpdateFunctionCodeParams = {
-    FunctionName: `${lambdaName}`,
+    FunctionName: `${hello-world-lambda}`,
     Publish: true,
-    ZipFile: read(`${cwd}/lambda/${lambdaName}/${lambdaName}.zip`)
+    ZipFile: read(`${cwd}/lambda/${hello-world-lambda}/${hello-world-lambda}.zip`)
   };
   console.log('Uploading code to lambda with params:', lambdaUpdateFunctionCodeParams);
   return lambdaUpdateFunctionCode(lambdaUpdateFunctionCodeParams);
